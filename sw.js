29be1b4211dcae2a3e6665d6f8c72f76e6d733e0
Frankie Bagnardi <f.bagnardi@gmail.com>
2018-01-15 03:31:55 -0700

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/high-redux/app-2df7cc19f548686f8bd9.js","4505ba2667a215bb36b9257fe42c4a65"],["/high-redux/app-7d5da547db17f89fb2bb.js","921e993b917d561584743688165e7469"],["/high-redux/app-f7a29b5defb4ec8120ad.js","5f72e37c198aaabd72dd2ad70b59a064"],["/high-redux/commons-3f8cdb987559d31a3a4e.js","dee41c978604596b7889a0f00db17227"],["/high-redux/index.html","a2f15c783368e9eb9641903dff289c12"],["/high-redux/offline-plugin-app-shell-fallback/index.html","73616adf7e07a02aa020432e89cc5ecc"]];
+var precacheConfig = [["/high-redux/app-2df7cc19f548686f8bd9.js","4505ba2667a215bb36b9257fe42c4a65"],["/high-redux/app-7d5da547db17f89fb2bb.js","921e993b917d561584743688165e7469"],["/high-redux/app-f7a29b5defb4ec8120ad.js","5f72e37c198aaabd72dd2ad70b59a064"],["/high-redux/commons-3f8cdb987559d31a3a4e.js","dee41c978604596b7889a0f00db17227"],["/high-redux/index.html","85167312f4c7e631ab65e16b848fa00f"],["/high-redux/offline-plugin-app-shell-fallback/index.html","a52f9bd0d58224453e53a5bfdbde97ce"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
